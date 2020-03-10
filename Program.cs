6565a662722bba6e465fe931c303e6439f1e0c6f
fateev <fateev@205-01>
2020-03-10 18:40:16 +0200

    Simply Clever

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp15
 {
@@ -10,6 +6,8 @@ namespace ConsoleApp15
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello GIT !");
+            Console.Read();
         }
     }
 }
